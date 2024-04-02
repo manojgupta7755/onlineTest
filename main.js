574488cc3abc88f89052154c454d760fe41c847a
manojgupta7755 <88585417+manojgupta7755@users.noreply.github.com>
2024-04-02 17:32:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+app.use(express.json())
+
+const connection =  mysql.createConnection({
+    user : "admin",
+    password : "NoTeDeSt^C10.6?SxwY882}",
+    host : "nodejs-technical-test.cm30rlobuoic.ap-southeast-1.rds.amazonaws.com",
+    port : "3306",
+    database : "conqtvms_dev"
+})
+
+const getDatails = (req, res) => {
+
+    const pageSize = req.query.pageSize ? req.query.pageSize : 10
+    const currentPage = req.query.currentPage ? req.query.currentPage : 1
+    const searchBy = req.query.searchBy ? req.query.searchBy : ''
+
+    const Limit = pageSize
+    const offset = (currentPage - 1) * pageSize
+    const query = `select * from ProductV2  Limit ${offset}, ${Limit}`
+    connection.query(query, (err, data) => {
+        if (err) {
+            res.status(500).json({ status: "failed" })
+        } else {
+            let filteredData =[]
+            if (searchBy) {
+                data.forEach(i => {
+                    if ((i.productName.includes(searchBy) || i.productImagesName.includes(searchBy) || i.productImagesUrls.includes(searchBy) || i.brandName.includes(searchBy) ||
+                        i.description.includes(searchBy) || i.itemCode.includes(searchBy) || i.itemType.includes(searchBy))) {
+                        filteredData.push(i)
+                    }
+
+                })
+            }
+
+            res.status(200).json(filteredData.length ? filteredData : data)
+        }
+    })
+}
+
+
+ app.get('/getdetails',(req,res)=>{
+    getDatails(req,res)
+ })
+
+
+
+
+app.listen('3000', ()=>{
+if(connection) {
+    console.log("db connected");
+}
+    console.log('server started' );
+})
